1b5a73ac27a6622a7121e3bd29aff877978bc051
MasilaMasila <41108633+MasilaMasila@users.noreply.github.com>
2018-07-13 12:31:05 +0530

    Create con.java

---
+++ con.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class con
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+     String a=sc.nextLine();
+         System.out.print(a.concat("."));
+     }
+    }
